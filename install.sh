996d87267d0818a9afe2b663f82ed7ac1c244e3f
Maifee Ul Asad <29339330+maifeeulasad@users.noreply.github.com>
2021-01-08 13:58:34 +0600

    added all commands together

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install git linux-headers-generic build-essential dkms
+git clone https://github.com/Mange/rtl8192eu-linux-driver
+cd rtl8192eu-linux-driver
+sudo dkms add .
+sudo dkms install rtl8192eu/1.0
+echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf
+echo -e "8192eu\n\nloop" | sudo tee /etc/modules
+echo "options 8192eu rtw_power_mgnt=0 rtw_enusbss=0" | sudo tee /etc/modprobe.d/8192eu.conf
+sudo update-grub; sudo update-initramfs -u
+systemctl reboot -i
